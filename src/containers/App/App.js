4ec3e71f6e4a2dde90288ef21f23e3126d3db20f
spandan12 <you@example.com>
2019-11-25 10:43:24 +0545

    folder structure created

---
+++ src/containers/App/App.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+
+  }
+
+  render(){
+    return (
+      <h1>welcome</h1>
+    )
+  }
+  
+}
+
+export default App;
